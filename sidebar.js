ce0024bd3c6a5376f94370fc06548a562fc79d35
Kristian Skibb Skibrek <kristianskibrek@protonmail.com>
2024-02-24 20:17:54 +0100

    poc moving through the list

--- sidebar.js
+++ sidebar.js
@@ -35,12 +35,50 @@ function addBookmarkElement(bookmark){
 setup()
 
 
-
-
 /* 
- * logic for paring the list and 
- * create a
+    * logic for paring the list and 
+    * create an event listener which listens to keypresses 
 */
+
+addEventListener("keypress", (event) => {
+    console.log(event.key)
+    if(event.key == "k"){
+        move(true)
+    }
+    else if(event.key == "j"){
+        move(false)
+    }
+});
+
+var listIndex = undefined
+
+// up is a boolean which decides if you are movin up or down
+function move(up){
+
+    if(listIndex == null){
+        listIndex = 0
+        console.log("listindex is null")
+    }
+    else if(up && listIndex != 0){
+        console.log("movin up")
+        listIndex -= 1
+    }
+    else if(!up && listIndex != 1000){ //find a way to detect bottom
+        console.log("movin down")
+        listIndex += 1
+    }
+
+    focusElement(listIndex, up)
+    
+}
+
+function focusElement(elementIndex, up){
+    let prevItem = up? elementIndex+1 : elementIndex-1
+    let bookmarkElements = document.getElementsByClassName("bookmarkElement")
+
+    bookmarkElements.item(elementIndex).classList.add("bookmarkElementFocused")
+    bookmarkElements.item(prevItem).classList.remove("bookmarkElementFocused")
+}
 
 
 
